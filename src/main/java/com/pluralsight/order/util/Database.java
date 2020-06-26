ee73226d40cb2dc71fa9c120cb15293cd0a26a20
rishabh-s94 <66481639+rishabh-s94@users.noreply.github.com>
2020-06-26 14:28:26 +0530

    Update Database.java

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -13,9 +13,9 @@ import java.sql.SQLException;
 public class Database {
     private static Database instance = null;
     private static boolean isInitialized = false;
-    private String url;
+    private String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
     private String user = "sa";
-    private String password;
+    private String password = "";
     /**
      * Private constructor
      */
@@ -51,7 +51,8 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager.getConnection(url, user, password);
+
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
